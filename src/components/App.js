da4b4d8b522685c3b8c20dd62c8f39ee4d7de654
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-12 17:34:22 +0530

    p :

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,36 @@
 import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 import "@babel/polyfill";
+import axios from "axios";
 
 const App = () => {
   const [data,setData]=useState("");
+  const [err,setErr]=useState("")
   useEffect(()=>{
 async function fetchdata(){
+  try{
   const res=await fetch(" https://dummyjson.com/products");
  const data=await res.json();
-  setData(data);
+  setData(data)}
+  catch(err)
+  {
+   setErr(err.message);
+  }
 
 }
 fetchdata();
   },[])
+  console.log(data.products)
   return (
+
     <div>
       {
-        data!=""?<div><h1>Data from Api</h1><p>{JSON.stringify(data)}</p></div>:<div>Loading...</div>
+        err?<div>{err}</div>:
+      
+      
+        data!=""?<div><h1>Data Fetched from API</h1><pre>{JSON.stringify(data,null,2)}</pre></div>:<div>Loading...</div>
+      
       }
-        
     </div>
   )
 }
