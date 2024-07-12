b0af54a0f9611b7e8a71f78ec7634321de3ea883
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-12 16:27:00 +0530

    p

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import "@babel/polyfill";
 
 const App = () => {
+  const [data,setData]=useState("");
+  useEffect(()=>{
+async function fetchdata(){
+  const res=await fetch(" https://dummyjson.com/products");
+ const data=await res.json();
+  setData(data);
+
+}
+fetchdata();
+  },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+      {
+        data!=""?<div><h1>Data from Api</h1><p>{JSON.stringify(data)}</p></div>:<div>Loading...</div>
+      }
+        
     </div>
   )
 }
